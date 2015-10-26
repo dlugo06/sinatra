7b7064e62b99ca500b427938acc5b8692d58e52b
Diego Lugo <diego.lugo.06@gmail.com>
2015-10-26 14:29:54 -0400

    Add stock ticker

--- server.rb
+++ server.rb
@@ -1,17 +1,26 @@
 require 'sinatra'
+require 'httparty'
+require 'nokogiri'
 
 get '/' do
-  File.read(File.join('public', 'index.html'))
-end
+  # File.read(File.join('public', 'index.html'))
 
-get '/this' do
-  "Hello World"
-end
+  request_url = "http://finance.yahoo.com/q?s=AAPL"
+  response = HTTParty.get(request_url)
+  dom = Nokogiri::HTML(response.body)
 
-get '/sinatra' do
-  "Hello Sinatra"
-end
 
-get '/jacqui' do
-  "Hello Jacqui"
+  ["Today's price for AAPL is $", dom.xpath("//span[@id='yfs_l84_aapl']").first.content]
 end
+#
+# get '/this' do
+#   "Hello World"
+# end
+#
+# get '/sinatra' do
+#   "Hello Sinatra"
+# end
+#
+# get '/jacqui' do
+#   "Hello Jacqui"
+# end
