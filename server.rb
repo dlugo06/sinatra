e8df10d284715afe2329ac72b5f5e86bd9c3422f
Diego Lugo <diego.lugo.06@gmail.com>
2015-10-27 14:40:57 -0400

    Ready for GitHub

--- server.rb
+++ server.rb
@@ -3,24 +3,14 @@ require 'httparty'
 require 'nokogiri'
 
 get '/' do
-  # File.read(File.join('public', 'index.html'))
+  File.read(File.join('public', 'hello.txt'))
+end
 
+get '/stock' do
   request_url = "http://finance.yahoo.com/q?s=AAPL"
   response = HTTParty.get(request_url)
   dom = Nokogiri::HTML(response.body)
 
-
-  ["Today's price for AAPL is $", dom.xpath("//span[@id='yfs_l84_aapl']").first.content]
+  a = dom.xpath("//span[@id='yfs_l84_aapl']").first.content
+  "Today's price for AAPL is $#{a}"
 end
-#
-# get '/this' do
-#   "Hello World"
-# end
-#
-# get '/sinatra' do
-#   "Hello Sinatra"
-# end
-#
-# get '/jacqui' do
-#   "Hello Jacqui"
-# end
